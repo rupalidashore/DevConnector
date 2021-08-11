4f001adcf5e5253d42a4f14d2f7ef52cfef2221c
rupalidashore <rupali.dashore@gmail.com>
2021-08-11 15:41:37 -0700

    errors handling in UI

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,19 @@
 
 import React , { Component } from 'react';
 import {BrowserRouter as Router, Route} from 'react-router-dom';
+import {Provider} from 'react-redux'
 import './App.css';
 import Footer from './components/layout/Footer';
 import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
 import Register from './components/auth/Register';
 import Login from './components/auth/Login';
+import store from './store';
 
 class App extends Component  {
  render() {
    return (
+     <Provider  store={store}>
      <Router>
     <div className="App">
       <Navbar />
@@ -23,7 +26,8 @@ class App extends Component  {
       <Footer />
     </div>
     </Router>
-  );
+   </Provider>
+   );
  }
 }
 
