e3db09c0d2da822e12ae26bb7df56f11813337da
rupalidashore <rupali.dashore@gmail.com>
2021-09-04 12:34:13 -0700

    change in store file

--- client/src/store.js
+++ client/src/store.js
@@ -8,7 +8,7 @@ const store = createStore(
   rootReducer,{},
   compose(
   applyMiddleware(...middleware),
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__() //debugging purpose 
+ // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__() //debugging purpose 
  )
   );
   
